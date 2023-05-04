794178972b5ac22b9f12e21c8663346b9be40aec
shubham khatik <40956270+shubhamkhatik@users.noreply.github.com>
2023-05-04 16:26:25 +0530

    product route added

--- src/routes/route.index.js
+++ src/routes/route.index.js
@@ -3,10 +3,12 @@ import authRoutes from "./auth.route.js";
 import couponRoutes from "./coupon.route.js";
 import collectionRoutes from "./collection.route.js";
 import orderRoutes from "./order.route.js";
+import productRoutes from "./product.route.js";
 const router = Router();
 router.use("/auth", authRoutes);
 router.use("/coupon", couponRoutes);
 router.use("/collection", collectionRoutes);
 router.use("/order", orderRoutes);
+router.use("/product", productRoutes);
 
 export default router;
